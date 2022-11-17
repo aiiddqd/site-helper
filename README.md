b3b3b23708b302c5dcd4e09dc3af0f8136a1636f
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-11-17 15:59:54 +0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Bot GitHub Telegram
 
+https://github.com/uptimizt/bot-gh-telegram
--- bot.ts
+++ bot.ts
@@ -1,11 +1,16 @@
 import { Bot } from "grammy";
 import dotenv from "dotenv";
+import { Client } from "@notionhq/client"
+import { Octokit, App } from "octokit";
 
 dotenv.config()
 
 const token:string = process.env.BOTTOKEN || '';
 
 const bot = new Bot(token);
+const github = new Octokit({ auth: process.env.TOKEN_GITHUB });
+const notion = new Client({ auth: process.env.NOTION_KEY });
+
 
 // You can now register listeners on your bot object `bot`.
 // grammY will call the listeners when users send messages to your bot.
