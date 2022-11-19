0f972f3ce2726e81a35597f02b7cdd93900d0668
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-11-19 16:42:39 +0300

    up

--- src/notion.js
+++ src/notion.js
@@ -6,37 +6,34 @@ dotenv.config()
 const databaseId = process.env.NOTION_DB
 const notionClient = new Client({ auth: process.env.NOTION_KEY })
 
-const notion = {
 
-    async addItem(title, children) {
+async function addItem(title, children) {
 
-        console.log(title, children)
+    console.log(title, children)
 
-        try {
-            const response = await notionClient.pages.create({
-                parent: { 
-                    "type": "database_id",
-                    "database_id": databaseId
-                },
-                properties: {
-                    Name: {
-                        title: [{ type: "text", text: { content: title } }]
-                    }
-                },
-                children: [...children]
-            })
+    try {
+        const response = await notionClient.pages.create({
+            parent: { 
+                "type": "database_id",
+                "database_id": databaseId
+            },
+            properties: {
+                Name: {
+                    title: [{ type: "text", text: { content: title } }]
+                }
+            },
+            children: [...children]
+        })
 
-            response.status = "success"
-            return response
-    
-        } catch(err){
-            console.log(err)
-            return {
-                status: "error"
-            }
+        response.status = "success"
+        return response
+
+    } catch(err){
+        console.log(err)
+        return {
+            status: "error"
         }
     }
-
 }
 
-module.exports = notion
+exports.addItem = addItem
