710d4463d129af87cea31835e596050cabe5638d
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2023-02-18 20:05:58 +0300

    save

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Plugin Name: @ Helper
+* Description: Helper for WordPress - additional options, tools, customizations
+* Author: uptimizt
+* GitHub Plugin URI: https://github.com/uptimizt/aa-helper
+* Version: 0.230218
+*/
+
+namespace ReAgg\LazyBlocks;
+
+$files = glob(__DIR__ . '/includes/*.php');
+foreach ($files as $file) {
+  require_once $file;
+}
