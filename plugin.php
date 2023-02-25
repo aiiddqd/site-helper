0de1b9bdd79bc54ede290df793041046012c6a95
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2023-02-25 13:33:57 +0300

    plugin meta

--- plugin.php
+++ plugin.php
@@ -3,8 +3,8 @@
  * Plugin Name: @ SiteHelper
  * Description: Site Helper for WordPress - additional options, tools, customizations, optimizations
  * Author: uptimizt
- * GitHub Plugin URI: https://github.com/uptimizt/aa-site-helper
- * Version: 0.230218
+ * GitHub Plugin URI: https://github.com/uptimizt/site-helper
+ * Version: 0.230225
  */
 
 namespace SiteHelper;
