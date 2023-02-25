45ed5831cd6fc641467aad990adaaa523b7d2ef9
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2023-02-25 13:30:55 +0300

    add Settings link to plugins list

--- plugin.php
+++ plugin.php
@@ -1,11 +1,11 @@
 <?php
 /*
-* Plugin Name: @ SiteHelper
-* Description: Site Helper for WordPress - additional options, tools, customizations, optimizations
-* Author: uptimizt
-* GitHub Plugin URI: https://github.com/uptimizt/aa-site-helper
-* Version: 0.230218
-*/
+ * Plugin Name: @ SiteHelper
+ * Description: Site Helper for WordPress - additional options, tools, customizations, optimizations
+ * Author: uptimizt
+ * GitHub Plugin URI: https://github.com/uptimizt/aa-site-helper
+ * Version: 0.230218
+ */
 
 namespace SiteHelper;
 
@@ -18,3 +18,32 @@ $files = glob(__DIR__ . '/includes/*/config.php');
 foreach ($files as $file) {
   require_once $file;
 }
+
+/**
+ * add Settings link to plugins list
+ */
+add_filter(
+  'plugin_action_links_' . plugin_basename(__FILE__),
+  function ($links) {
+    if (!current_user_can('manage_options')) {
+      return $links;
+    }
+
+    return array_merge(
+      $links,
+      array(
+        sprintf(
+          '<a href="%s">%s</a>',
+          add_query_arg(
+            array(
+              'page' => \SiteHelper\Config\OPTION_PAGE,
+            ),
+            admin_url('options-general.php')
+          ),
+          esc_attr__('Settings', 'antispam-bee')
+        ),
+      )
+    );
+
+  }
+);
